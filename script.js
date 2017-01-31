bada7b03895354551105f08213da30a551922429
Nat Carter <n.goldman.carter@gmail.com>
2017-01-31 14:54:04 -0500

    changed quite a bit; lost track

--- script.js
+++ script.js
@@ -1,53 +1,90 @@
-var modifiers = ['meta', 'shift', 'ctrl', 'alt']
+var mods = ['meta', 'shift', 'ctrl', 'alt']
 
-var shortcuts = [
-  ['toggle comment', 'meta /'],
-  ['duplicate line', 'meta shift d'],
-  ['move line up', 'meta ctrl arrowup'],
-  ['move line down', 'meta ctrl arrowdown'],
-  ['indent', 'meta ]'],
-  ['outdent', 'meta ['],
-  ['toggle tree view', 'meta \\']
+var cuts = [
+  ['indent', 'cmd+]'],
+  ['outdent', 'cmd+['],
+  ['toggle comment', 'cmd+/'],
+  ['toggle tree view', 'cmd+\\'],
+  ['select current word', 'cmd+d'],
+  ['select current line', 'cmd+l'],
+  ['duplicate line', 'cmd+shift+d'],
+  ['move line up', 'cmd+ctrl+arrowup'],
+  ['move line down', 'cmd+ctrl+arrowdown']
 ];
 
-var prompt = $('#prompt');
-var attempt = '';
-var target = '';
+var indexes = [];
 
-setTarget();
+fillIndexes();
 
-function setTarget() {
-  var i = Math.floor(Math.random()*shortcuts.length);
-  attempt = '';
-  target = shortcuts[i][1];
-  prompt.html(shortcuts[i][0])
+function fillIndexes() {
+  for (var i = 0; i < cuts.length; i++) {
+    indexes.push(i);
+  }
 }
 
+var target;
+a = $('#answer');
+p = $('#prompt');
+
+getTarget();
+
 $(document).keydown(function(e) {
-  if (e.metaKey && e.key === 'r') {
-    return;
-  }
-  e.preventDefault();
-  if ([16, 17, 18, 91, 93].indexOf(e.which) !== -1) {
-    return;
-  }
-  for (var i = 0; i < modifiers.length; i++) {
-    if (e[modifiers[i]+'Key']) {
-      attempt += modifiers[i]+' ';
+  if (needsHearing(e)) {
+    e.preventDefault();
+    var input = getInput(e);
+    if (input === target) {
+      handleRight();
+    } else {
+      handleWrong();
     }
   }
-  attempt += e.key.toLowerCase();
-  if (attempt === target) {
-    prompt.attr('class', 'right');
-    setTimeout(function() {
-      prompt.attr('class', '');
-      setTarget();
-    }, 1000);
-  } else {
-    attempt = '';
-    prompt.attr('class', 'wrong');
-    setTimeout(function() {
-      prompt.attr('class', '');
-    }, 1000);
-  }
 });
+
+function getTarget() {
+  var i = Math.floor(Math.random()*indexes.length);
+  var j = indexes[i];
+  indexes.splice(i, 1);
+  if (indexes.length === 0) {
+    fillIndexes();
+  }
+  p.html(cuts[j][0]);
+  target = cuts[j][1];
+}
+
+function needsHearing(e) {
+  if ([16, 17, 18, 91, 93].indexOf(e.which) !== -1) {
+    return false;
+  } else if (e.metaKey && e.key === 'r') {
+    return false;
+  }
+  return true;
+}
+
+function getInput(e) {
+  var input = '';
+  for (var i = 0; i < mods.length; i++) {
+    var mod = mods[i];
+    if (e[mod+'Key']) {
+      if (mod === 'meta') {
+        mod = 'cmd';
+      }
+      input += mod+'+';
+    }
+  }
+  input += e.key.toLowerCase();
+  return input;
+}
+
+function handleRight() {
+  p.attr('class', 'right');
+  setTimeout(function() {
+    p.attr('class', '');
+    a.html('');
+    getTarget();
+  }, 1000);
+}
+
+function handleWrong() {
+  p.attr('class', 'wrong');
+  a.html(target);
+}
